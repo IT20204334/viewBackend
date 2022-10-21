9c00801dae4099a8c8463b324799f8b34a80a926
Bodhitha Weerasekara <88415912+IT20204334@users.noreply.github.com>
2022-10-21 18:47:59 +0530

    Delete src directory

--- src/Routes/view.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const userCotoller = require("../controllers/view.controller");
-
-router.post("/", userCotoller.createView);
-router.get("/", userCotoller.getAllViews);
-
-module.exports = router;
